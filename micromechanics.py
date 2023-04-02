b05bcc10f8c20306356702af5c07aa06a4f14d4b
ณพงศ์กร สุวรรณประทีป <napongkorn_suv@gmail.com>
2023-04-02 00:46:20 -0700

    add copy of Halpin Tsai Code

--- micromechanics.py
+++ micromechanics.py
@@ -36,14 +36,36 @@ def calculate_G(G_f:float, G_m:float, V_f:float):
     #V_f: Volume fraction of fiber
     return (G_f*G_m)/(G_m*V_f + (1-V_f)*G_f)
 
-class Material:
-    def __init__(self,name: str, modulus: float, poisson_ratio:float) -> None:
-        self.name = name
-        self.modulus = modulus
-        self.poisson_ratio = poisson_ratio
-        pass
+#TODO: Halpin-Tsai Model for UD and random and Randomly Orient
+# #Halpin-Tsai : Unidirectional Continuous
+# def cal_halpinTsai(Em,Ef,Vf,psi):
+#   E_ratio = Ef/Em
+#   eta = (E_ratio-1)/(E_ratio+psi)
+#   return (Em*(1+psi*eta*Vf))/(1-eta*Vf)
 
-class lamina:
-    def __init__(self,fiberMaterial,) -> None:
-        pass
+# #Halpin-Tsai Unidirectional Calculation
+# HT_E1 = cal_halpinTsai(3.5,72,0.4,100)
+# HT_E2 = cal_halpinTsai(3.5,72,0.4,2)
+# print(HT_E1)
+# print(HT_E2)
 
+# #Unidirectional Discontinuous
+# def cal_MatProp_UD(Em,Ef,Vf,AR):
+#   #This function calculate E1,E2 of a Unidirectional Discontinuous fiber lamina. It assume isotropy in Matrix and Fiber.
+#   #TODO: add G12,v12 calculation
+#   E_ratio = Ef/Em
+#   etaL = (E_ratio-1)/(E_ratio+2*AR)
+#   etaT = (E_ratio-1)/(E_ratio+2)
+#   E1 = (Em*(1+2*AR*etaL*Vf))/(1-etaL*Vf)
+#   E2 = (Em*(1+2*etaT*Vf))/(1-etaT*Vf)
+#   return E1,E2
+
+# #Randomly Oriented Discontinuous Fiber Lamina
+# def cal_MatProp_RD(Em,Ef,Vf,AR):
+#     #This function calculate E1,E2 of a Randomly Oriented Discontinuous Fiber Lamina. It assume isotropy in Matrix and Fiber.
+#     #A thin lamina containing randomly oriented discontinuous fibers exhibits planar isotropic behavior
+#     E1,E2 = cal_MatProp_UD(Em,Ef,Vf,AR)
+#     E_rand = (3/8)*E1 + (5/8)*E2
+#     G_rand = (1/8)*E1 + (1/4)*E2
+#     v_rand = (E_rand/(2*G_rand))-1
+#     return E_rand,G_rand,v_rand
